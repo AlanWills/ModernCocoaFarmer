1db57c050c97cbf235a19ad557d1cd3ab8fb1e80
Alan Wills <alawills@googlemail.com>
2019-04-27 03:02:54 +0100

    Trying to fix MCF project

--- ModernCocoaFarmer/Source/Main.cpp
+++ ModernCocoaFarmer/Source/Main.cpp
@@ -1,4 +1,4 @@
-#include "MCFGame.h"
+#include "Debugging/Profiling/Profiler.h"
 
 // Disables console window
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
@@ -9,9 +9,5 @@
 // The MAIN function, from here we start the application and run the game loop
 int main()
 {
-	// Don't need to use unique_ptr as it will be deleted through 'current' unique_ptr
-  MCF::MCFGame* game = new MCF::MCFGame();
-	game->run();
-
 	return 0;
 }
