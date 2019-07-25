ee62438f6d6727451c6a1cfe2926611d921555d1
Alan Wills <alawills@googlemail.com>
2019-07-26 00:13:06 +0100

    Trying to work on continuous sending of data

--- ModernCocoaFarmer/Source/Main.cpp
+++ ModernCocoaFarmer/Source/Main.cpp
@@ -2,6 +2,11 @@
 #include "Lua/ScriptCommands/MCFScriptCommands.h"
 #include "UtilityHeaders/PlatformHeaders.h"
 
+#include "curlpp/curl_easy.h"
+#include "curlpp/curl_form.h"
+#include "curlpp/curl_ios.h"
+#include "curlpp/curl_exception.h"
+
 // Disables console window
 #if WINDOWS && !defined(__CYGWIN__)
 #ifndef _DEBUG
@@ -10,14 +15,59 @@
 #endif
 
 
-// The MAIN function, from here we start the application and run the game loop
+std::stringstream get_response(std::string url)
+{
+  std::stringstream str;
+  try
+  {
+    curl::curl_ios<std::stringstream> writer(str);
+    curl::curl_easy easy(writer);
+
+    easy.add<CURLOPT_URL>(url.data());
+    easy.add<CURLOPT_VERBOSE>(true);
+
+    struct curl_slist* headers = NULL;
+    headers = curl_slist_append(headers, "Content-Type: application/octet-stream");
+    easy.add<CURLOPT_HTTPHEADER>(headers);
+
+    easy.add<CURLOPT_POSTFIELDS>("abcd");
+    easy.add<CURLOPT_POSTFIELDSIZE>(5);
+    easy.add<CURLOPT_PORT>(13000);
+
+    easy.perform();
+  }
+  catch (curl::curl_easy_exception const& error)
+  {
+    auto errors = error.get_traceback();
+    error.print_traceback();
+  }
+
+  return str;
+}
+
 int main()
 {
-  // Set up game specific script commands
-  MCF::Lua::MCFScriptCommands::initialize();
+  while (true)
+  {
+    using namespace std::string_literals;
 
-  CelesteEngine::Game* game = new CelesteEngine::Game();
-  game->run();
-  
+    auto url = "http://localhost/";
+    auto json = get_response(url);
+
+    std::cout << json.str() << std::endl;
+  }
+
   return 0;
 }
+
+// The MAIN function, from here we start the application and run the game loop
+//int main()
+//{
+//  // Set up game specific script commands
+//  MCF::Lua::MCFScriptCommands::initialize();
+//
+//  CelesteEngine::Game* game = new CelesteEngine::Game();
+//  game->run();
+//  
+//  return 0;
+//}
