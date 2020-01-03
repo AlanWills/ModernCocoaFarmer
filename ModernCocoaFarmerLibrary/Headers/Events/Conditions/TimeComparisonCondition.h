c86372d8a377ae9e1b0d63d24ed8f6c8a8edcbc2
Alan Wills <alawills@googlemail.com>
2020-01-03 23:47:45 +0000

    Updating timecomparisoncondition

--- ModernCocoaFarmerLibrary/Headers/Events/Conditions/TimeComparisonCondition.h
+++ ModernCocoaFarmerLibrary/Headers/Events/Conditions/TimeComparisonCondition.h
@@ -1,5 +1,38 @@
 #pragma once
 
+#include "MCFLibraryDllExport.h"
+#include "Events/Conditions/ICondition.h"
+
 // Day/Month/Year enum
 // Operator enum
 // Value int
+
+namespace MCF::Events::Conditions
+{
+  class TimeComparisonCondition : public ICondition
+  {
+    DECLARE_SCRIPTABLE_OBJECT(TimeComparisonCondition, MCFLibraryDllExport);
+
+    public:
+      enum class Period
+      {
+        kDay,
+        kMonth,
+        kYear
+      };
+
+      enum class Operator
+      {
+        kLessThan,
+        kLessThanOrEqualTo,
+        kEqual,
+        kGreaterThanOrEqualTo,
+        kGreaterThan
+      };
+
+    private:
+      CelesteEngine::ValueField<Period>& m_period;
+      CelesteEngine::ValueField<Operator>& m_operator;
+      CelesteEngine::ValueField<int>& m_value;
+  };
+}
