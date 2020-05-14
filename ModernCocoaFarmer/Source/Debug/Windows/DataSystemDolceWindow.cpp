fdf2b110d250bc87075f587f89755d261bdf312d
Alan Wills <alawills@googlemail.com>
2020-05-14 21:40:19 +0100

    Starting to get variant functions working

--- ModernCocoaFarmer/Source/Debug/Windows/DataSystemDolceWindow.cpp
+++ ModernCocoaFarmer/Source/Debug/Windows/DataSystemDolceWindow.cpp
@@ -8,14 +8,19 @@ namespace MCF::Debug
 {
   //------------------------------------------------------------------------------------------------
   using Data = Persistence::DataStore::Data;
-  using RenderFunction = void (*)(const std::string&, const Data&, Persistence::DataStore&);
 
-  struct RenderFunctions
+  template <class VariantType, typename ReturnType, typename... Args>
+  struct VariantFunctions
   {
-    constexpr size_t size() const { return std::variant_size<Data>(); }
+    using Variant = VariantType;
+    using Function = ReturnType(*)(Args...);
 
-    RenderFunction m_functions[std::variant_size<Data>()];
+    constexpr size_t size() const { return std::variant_size<Data>::value; }
+
+    Function m_functions[std::variant_size<Data>::value];
   };
+
+  using RenderFunctions = VariantFunctions<Data, void, const std::string&, const Data&, Persistence::DataStore&>;
 
   bool render(const std::string& key, bool& value)
   {
@@ -50,38 +55,48 @@ namespace MCF::Debug
   }
 
   template <typename T>
-  void render(const std::string& key, const Data& data, Persistence::DataStore& dataStore)
+  struct RenderFunctor
   {
-    T tempData = std::get<T>(data);
-
-    if (render(key, tempData))
+    static void execute(const std::string& key, const Data& data, Persistence::DataStore& dataStore)
     {
-      dataStore.set(key, tempData);
+      T tempData = std::get<T>(data);
+
+      if (render(key, tempData))
+      {
+        dataStore.set(key, tempData);
+      }
     }
-  }
+  };
 
-  template <size_t index>
-  constexpr void setRenderFunction(RenderFunctions& renderFunctions)
+  template <typename VariantFunctions, size_t index>
+  struct SetVariantFunction
   {
-    renderFunctions.m_functions[index] = &render<typename std::variant_alternative<index, Data>::type>;
-    setRenderFunction<index - 1>(renderFunctions);
-  }
+    static constexpr void _(VariantFunctions& renderFunctions)
+    {
+      renderFunctions.m_functions[index] = &RenderFunctor<typename std::variant_alternative<index, typename VariantFunctions::Variant>::type>::execute;
+      SetVariantFunction<VariantFunctions, index - 1>::_(renderFunctions);
+    }
+  };
 
-  template <>
-  constexpr void setRenderFunction<0>(RenderFunctions& renderFunctions)
+  template <typename VariantFunctions>
+  struct SetVariantFunction<VariantFunctions, 0>
   {
-    renderFunctions.m_functions[0] = &render<typename std::variant_alternative<0, Data>::type>;
-  }
+    static constexpr void _(VariantFunctions& renderFunctions)
+    {
+      renderFunctions.m_functions[0] = &RenderFunctor<typename std::variant_alternative<0, typename VariantFunctions::Variant>::type>::execute;
+    }
+  };
 
-  constexpr RenderFunctions createRenderFunctions()
+  template <typename VariantFunctions>
+  constexpr VariantFunctions createVariantFunctions()
   {
-    RenderFunctions renderFunctions = RenderFunctions();
-    setRenderFunction<renderFunctions.size() - 1>(renderFunctions);
+    VariantFunctions variantFunctions = VariantFunctions();
+    SetVariantFunction<VariantFunctions, variantFunctions.size() - 1>::_(variantFunctions);
 
-    return renderFunctions;
+    return variantFunctions;
   }
 
-  static RenderFunctions s_renderFunctions = createRenderFunctions();
+  static RenderFunctions s_renderFunctions = createVariantFunctions<RenderFunctions>();
 
   //------------------------------------------------------------------------------------------------
   DataSystemDolceWindow::DataSystemDolceWindow(Persistence::DataSystem& dataSystem) :
